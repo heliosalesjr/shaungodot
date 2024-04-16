6c0eab6385c393d16876a22453691930e02b3ddb
heliosalesjr <heliosalesjr@gmail.com>
2024-04-16 20:21:15 -0300

    bullet offset

--- scenes/level.gd
+++ scenes/level.gd
@@ -5,6 +5,7 @@ const bullet_scene: PackedScene = preload("res://scenes/bullet.tscn")
 func _on_player_shoot(pos, facing_right):
 	var bullet = bullet_scene.instantiate()
 	var direction = 1 if facing_right else -1
-	$Bullets.add_child(bullet)
-	bullet.position = pos
 	bullet.direction = direction
+	$Bullets.add_child(bullet)
+	bullet.position = pos + Vector2(16 * direction,0)
+	
